e8ed36f354f7a9bfd6831a7ed78ea998eebee59c
Simoh13 <s.yelleschaouche@esi-sba.dz>
2023-01-12 16:35:11 +0100

    Added Subscribe section with category filtering

--- client/src/scenes/home/ShoppingList.jsx
+++ client/src/scenes/home/ShoppingList.jsx
@@ -53,6 +53,46 @@ const ShoppingList = () => {
             >
                 Our featured <b>Products</b>
             </Typography>
+            <Tabs
+            textColor="primary"
+            indicatorColor="primary"
+            value={value}
+            onChange={handleChange}
+            centered
+            TabIndicatorProps={{sx : {display: isNonMobile ? "block" : "none"}}}
+            sx={{
+                m: "25px",
+                "& .MuiTabs-flexContainer": {
+                    flexWrap: "wrap"
+                }
+            }}
+            >
+                <Tab label="All" value="all"/>
+                <Tab label="NEW ARRIVALS" value="newArrivals"/>
+                <Tab label="BEST SELLERS" value="bestSellers"/>
+                <Tab label="TOP RATED" value="topRated"/>
+            </Tabs>
+            <Box
+            margin="0 auto"
+            display="grid"
+            gridTemplateColumns="repeat(auto-fill, 300px)"
+            justifyContent="space-around"
+            rowGap="20px"
+            columnGap="1.33%"
+            >
+                {value === "all" && items.map((item) => (
+                    <Item item={item} key={`${item.name}-${item.id}`}/>
+                ))}
+                {value === "newArrivals" && newArrivalsItems.map((item) => (
+                    <Item item={item} key={`${item.name}-${item.id}`}/>
+                ))}
+                {value === "bestSellers" && bestSellersItems.map((item) => (
+                    <Item item={item} key={`${item.name}-${item.id}`}/>
+                ))}
+                {value === "topRated" && topRatedItems.map((item) => (
+                    <Item item={item} key={`${item.name}-${item.id}`}/>
+                ))}
+            </Box>
         </Box>
     )
 }
